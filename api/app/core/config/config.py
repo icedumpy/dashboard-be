5decfa0a3b24ef65cb6b9404e106358341db6389
Pongsakorn DataWow <pongsakorn.p@datawow.io>
2025-08-25 09:01:31 +0700

    chore: increase access token

--- api/app/core/config/config.py
+++ api/app/core/config/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    JWT_SECRET: str = "dev"
+    JWT_SECRET: str = "fitesadev"
     JWT_ALG: str = "HS256"
-    ACCESS_TOKEN_MIN: int = 30
+    ACCESS_TOKEN_MIN: int = 60
     REFRESH_TOKEN_DAYS: int = 7
     IMAGES_DIR: str = 'images'
 
