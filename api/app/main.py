ec9bf60a2e62ec7a891cb894f16bbeb8747dfbac
Pongsakorn DataWow <pongsakorn.p@datawow.io>
2025-09-02 17:12:59 +0700

    chore: allow internal ip

--- api/app/main.py
+++ api/app/main.py
@@ -18,6 +18,8 @@ REDOC_PATH = "/redoc"
 ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:4173",
+    "http://192.168.10.200:5173/",
+    "http://192.168.10.200:4173/",
 ]
 
 app = FastAPI(
